6dfa01163c7cde34ed231c9ededfd2085d0e003f
stgleb <glebstepanov1992@gmail.com>
2017-03-08 15:45:08 +0200

    Update percentiles for statsd

--- statsd/config.js
+++ statsd/config.js
@@ -1,8 +1,8 @@
-{
+[B{
   port: 8125,
   mgmt_port: 8126,
 
-  percentThreshold: [1, 2, 5, 10, 25, 50, 75, 90, 95, 98, 99, 99.9, 99.99, 99.999],
+  percentThreshold: [50, 75, 90, 95, 98, 99, 99.9, 99.99, 99.999],
 
   graphitePort: 2003,
   graphiteHost: "127.0.0.1",
