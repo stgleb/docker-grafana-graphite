010807859b61fe55a7e7b33d88716bdaf77a8228
stgleb <glebstepanov1992@gmail.com>
2017-08-23 16:05:41 +0300

    Enable IPv6 for statsd

--- statsd/config.js
+++ statsd/config.js
@@ -1,11 +1,13 @@
 {
+  address: "::1",
+  address_ipv6: true,
   port: 8125,
   mgmt_port: 8126,
 
   percentThreshold: [50, 75, 90, 95, 98, 99, 99.9, 99.99, 99.999],
 
   graphitePort: 2003,
-  graphiteHost: "127.0.0.1",
+  graphiteHost: "0.0.0.0",
   flushInterval: 10000,
 
   backends: ['./backends/graphite'],
@@ -13,3 +15,4 @@
     legacyNamespace: false
   }
 }
+
